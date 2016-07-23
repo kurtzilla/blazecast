d9f00cb5787cd2dfdef4458e911a7b0e3b868243
Josh Newsom <newsommeister@gmail.com>
2016-07-23 15:37:22 -0600

    Make migration for creating users_podcasts table

---
+++ migrations/20160723153011_add_users_podcasts_table.js
@@ -0,0 +1,13 @@
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('users_podcasts', function(table) {
+        table.increments();
+        table.integer('user_id').references('users.id');
+        table.integer('podcast_id').references('podcasts.id');
+        table.boolean('favorite');
+        table.integer('rating');
+    });
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTable('users_podcasts')
+};
