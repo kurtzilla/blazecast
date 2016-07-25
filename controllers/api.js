c5a4eac273bfd4da26e8aca2c6e33f5f3e73333a
rob kurtz <rob@robkurtz.net>
2016-07-24 18:35:31 -0600

    cleaned up api.js route

--- controllers/api.js
+++ controllers/api.js
@@ -1,45 +1,4 @@
 
-// var dotenv = require('dotenv');
-// var jwt = require('jsonwebtoken');
-var https = require('https');
-
-// Load environment variables from .env file
-// dotenv.load();
-
-var request = require('request');
-// request('http://www.google.com', function (error, response, body) {
-//   if (!error && response.statusCode == 200) {
-//     console.log(body) // Print the google web page.
-//   }
-// })
-
-
-exports.getPodcast = function(req, res, next){
-  var provider_id = req.params.id;
-  console.log('prov id', provider_id);
-
-  //return Promise.resolve('pinged api func');
-
-  // TODO ping database for our version - then if not found - go to itunes
-
-  // goto itunes to retrieve podcast and episode list
-  var url = 'https://itunes.apple.com/lookup?collectionId=' + provider_id.toString();
-  request(url, function(err, res, body){
-    console.log('cll return', err);
-    console.log('cll return', body);
-    if(err){
-      return res.json(err);
-    }
-    if (!err && res.statusCode == 200) {
-      console.log('BODY Response', body);
-      return res.json(body);
-    }
-
-  });
-
-
-}
-
 
 
 /* This portion of the api will only return non-sensitive key values */
