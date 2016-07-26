182e32ab770f36bb08f33fd178b33781ab8e5747
Kirsten <kirstenwedde@gmail.com>
2016-07-26 16:42:25 -0600

    created browse page and began the filtering process

--- public/js/controllers/browse.js
+++ public/js/controllers/browse.js
@@ -1,10 +1,18 @@
 angular.module('MyApp')
-.controller('BrowseCtrl', function($scope, $location) {
+.controller('BrowseCtrl', function($scope, $location, $http, $stateParams) {
 
   $scope.view = {};
   $scope.view.showId = "";
 
+  $http.get('/itunesdummydata').then(function(data){
+    console.log(data.data.results);
+    $scope.view.podcasts = data.data.results;
+    
+  });
+
   $scope.showPage = function(id){
+    console.log('clicked');
+    // location.assign('/show/' + id);
     $location.path('/show/' + id);
   }
 
