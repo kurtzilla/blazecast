1971df64dcc395f73617735e611ba9065c2ae199
Josh Newsom <newsommeister@gmail.com>
2016-07-26 00:16:48 -0600

    Bare bones dashboard

--- public/js/controllers/dashboard.js
+++ public/js/controllers/dashboard.js
@@ -1,7 +1,19 @@
 
 
-app.controller('DashboardCtrl', function($scope) {
+app.controller('DashboardCtrl', function ($scope, $rootScope, $http) {
+  var user = $rootScope.currentUser;
 
-  $scope.view = {};
+  $scope.view = {
+    userName: user.name
+  };
+
+  $http.get('/api/users/' + user.id + '/dashboard')
+    .then(function (data) {
+      console.log(data);
+      $scope.view.favorites = data.data;
+    })
+
+  // console.log(user);
+
 
 });
