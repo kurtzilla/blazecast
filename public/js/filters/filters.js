97b7ead80ff436122c9c4d0b0a3036976bcf92d7
rob kurtz <rob@robkurtz.net>
2016-07-27 08:42:57 -0600

    updated protocol filter to be generic - aka heroku

--- public/js/filters/filters.js
+++ public/js/filters/filters.js
@@ -1,19 +1,22 @@
 app.filter('formatProtocol', ['$location', function($location){
    return function(input){
      if(input){
-       var currentProtocol = $location.$$protocol;
-       var inp = '';
 
-       if(currentProtocol === "https" && input.toLowerCase().indexOf('http:') !== -1){
-         inp = input.replace(/^http:\/\//g, currentProtocol + '://')
-       } else {
-         inp = input;
-       }
-
-      //  console.log('PPP',currentProtocol, input);
-      //  var inp = input.replace(/^http:\/\//g, currentProtocol + '://')
-      //    .replace(/^https:\/\//g, currentProtocol + '://');
-       return inp;
+       return input.replace(/^http:\/\//g, '//')
+          .replace(/^https:\/\//g, '//');
+      //
+      //  var currentProtocol = $location.$$protocol;
+      //  var inp = '';
+      //
+      //  if(currentProtocol === "https" && input.toLowerCase().indexOf('http:') !== -1){
+      //    inp = input.replace(/^http:\/\//g, currentProtocol + '://')
+      //  } else {
+      //    inp = input;
+      //  }
+      //
+      // //  var inp = input.replace(/^http:\/\//g, currentProtocol + '://')
+      // //    .replace(/^https:\/\//g, currentProtocol + '://');
+      //  return inp;
      }
   };
 }]);
