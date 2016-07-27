03daf59159e5f9a24278f8b27f1ffd4be6952ac1
Josh Newsom <newsommeister@gmail.com>
2016-07-26 19:33:19 -0600

    Clicking follow toggles follow status

--- controllers/api.js
+++ controllers/api.js
@@ -11,6 +11,7 @@ exports.followPodcast = function (req, res, next) {
   var podcastName = req.body.podcastName;
   var feedUrl = req.body.feedUrl;
   var images = req.body.images;
+  var podcastId;
 
   // first, check to see if podcast is already in database
   knex('podcasts')
@@ -29,33 +30,35 @@ exports.followPodcast = function (req, res, next) {
         return new Promise((resolve, reject) => {resolve([data[0].id])}); // return a promise to preserve chain
       }
     })
-    .then(function(data) {
-      var podcastId = data[0];
+    .then(function(data) { // check to see if podcast is already followed by this user
+      podcastId = data[0];
       return knex('users_podcasts')
-
+        .where('user_id', userId)
+        .andWhere('podcast_id', podcastId);
+    })
+    .then(function(data) {
+      var following = data[0].following;
+      console.log('following:', following);
+      if (!data.length) {
+        return knex('users_podcasts')
         .insert({
           user_id: userId,
           podcast_id: podcastId,
           following: true
-        })
-
+        });
+      } else {
+        return knex('users_podcasts')
+          .update({
+            following: !following
+          })
+          .where('podcast_id', podcastId);
+      }
     })
     .then(function(data) {
+      console.log('data2:', data);
       res.end();
-    })
+    });
 }
-
-// exports.getUserDashboard = function (req, res, next) {
-//   knex.queryBuilder()
-//     .select('podcasts.images', 'podcasts.name', 'podcasts.feedUrl')
-//     .from('podcasts')
-//     .innerJoin('users_podcasts', 'podcasts.id', 'podcast_id')
-//     .innerJoin('users', 'users.id', 'user_id')
-//     .where('users.id', req.params.user_id)
-//     .then(function(data) {
-//       console.log(data)
-//       res.json(data);
-//     });
 
 exports.getFollows = function(req, res, next) {
   var userId = req.params.user_id;
@@ -68,18 +71,6 @@ exports.getFollows = function(req, res, next) {
     })
 
 };
-
-// exports.getUserDashboard = function (req, res, next) {
-//   knex.queryBuilder()
-//     .select('podcasts.name')
-//     .from('podcasts')
-//     .innerJoin('users_podcasts', 'podcasts.id', 'podcast_id')
-//     .innerJoin('users', 'users.id', 'user_id')
-//     .where('users.id', req.params.user_id)
-//     .then(function(data) {
-//       res.json(data);
-//     });
-// };
 
 /* This portion of the api will only return non-sensitive key values */
 //
