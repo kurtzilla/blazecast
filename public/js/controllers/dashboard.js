43d2d5e4625838701879febce961da3ba559b4d6
Kirsten <kirstenwedde@gmail.com>
2016-07-29 13:11:57 -0600

    Fixed everything

--- public/js/controllers/dashboard.js
+++ public/js/controllers/dashboard.js
@@ -52,12 +52,11 @@ app.controller('DashboardCtrl', function($scope, $rootScope, $http, $stateParams
     $http.post('/api/users/' + user.id + '/save/' + providerId + '/' + itunesEpisodeId)
       .then(function(){});
   };
-
   $scope.showSaved = function () {
     $http.get('/api/users/' + user.id + '/savedPodcasts')
-      .then(function(data) {
+      .then(function(data){
         var savedEps = data.data;
-
+        $scope.view.episodes = savedEps;
         return episodeService.populateEpisodesByEpisodeId(savedEps)
       })
       .then(function (promises) {
@@ -71,8 +70,6 @@ app.controller('DashboardCtrl', function($scope, $rootScope, $http, $stateParams
             }
             $scope.$digest();
           })
-
-        // console.log(data);
       })
   };
 
@@ -80,7 +77,19 @@ app.controller('DashboardCtrl', function($scope, $rootScope, $http, $stateParams
     $http.get('/api/users/' + user.id + '/favoriteEpisodes')
       .then(function(data) {
         var faveEps = data.data;
-        $scope.view.episodes = faveEps;
+        return episodeService.populateEpisodesByEpisodeId(faveEps)
+      })
+      .then(function (promises) {
+        Promise.all(promises)
+          .then(function (data) {
+            $scope.view.episodes = [];
+            for (var i = 0; i < data.length; i++) {
+              data[i].data.audio_url = data[i].data.audio_files[0].url[0];
+              data[i].data.type = 'audio/mpeg';
+              $scope.view.episodes.push(data[i].data)
+            }
+            $scope.$digest();
+          })
       })
   }
 
